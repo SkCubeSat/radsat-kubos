8d9082423b6e73555e199a7c598ebdb43d586333
alvanhub <54913106+alvanhub@users.noreply.github.com>
2025-01-14 18:50:10 -0600

    compilation test

--- services/clyde-3g-eps-service/src/main.rs
+++ services/clyde-3g-eps-service/src/main.rs
@@ -12,7 +12,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//
+// 
+// MADE BY LAMB
 
 //! Service for interacting with a [Clyde Space 3G EPS](https://www.aac-clyde.space/satellite-bits/eps)
 //!
