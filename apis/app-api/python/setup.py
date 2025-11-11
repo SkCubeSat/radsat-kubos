a484a3a4edd4875bbba8126f20b41832f3ac37cd
alvanhub <54913106+alvanhub@users.noreply.github.com>
2025-11-11 17:06:41 -0600

    Fixing app_api again, using find_packages

--- apis/app-api/python/setup.py
+++ apis/app-api/python/setup.py
@@ -5,11 +5,11 @@ See:
 https://github.com/pypa/sampleproject
 """
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='kubos_app',
       version='0.1.0',
       description='Mission Application API for KubOS',
-      py_modules=["kubos_app"],
+      packages=find_packages(),
       install_requires=['toml']
       )
